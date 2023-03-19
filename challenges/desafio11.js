04f6691cec9a087dacf941facc99cf46dc92cb98
Deborah Cristina de Oliveira <cris_yumi02@hotmail.com>
2023-03-19 19:04:33 -0300

    req 4 ao 10

---
+++ challenges/desafio11.js
@@ -0,0 +1,2 @@
+db.produtos.find({ nome: { $nin: [/Big Mac/i, /McChicken/i] } },
+{ _id: 0, nome: 1, curtidas: 1, vendidos: 1 });
---
+++ challenges/desafio9.js
@@ -0,0 +1,4 @@
+db.produtos.find({ $and: [
+  { "valoresNutricionais.tipo": "calorias" },
+  { "valoresNutricionais.quantidade": { $lt: 500 } }] },
+    { _id: 0, nome: 1 }).skip(2);
